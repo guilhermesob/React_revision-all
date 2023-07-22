b37c69ae2e06c661281973dbc44a589d7f53e679
guilhermesob <101477908+guilhermesob@users.noreply.github.com>
2023-07-22 16:28:28 -0300

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <h1>Hello, Learner.Welcome to GeeksforGeeks.</h1>
+                <h1>Hello, Learner.Welcome to React.</h1>
             </div>
         );
     }

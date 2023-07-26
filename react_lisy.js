29e922b0dc2c00bd8b7a8fec0b6e035d6362b9f1
guilhermesob <101477908+guilhermesob@users.noreply.github.com>
2023-07-26 01:15:01 -0300

    Create react_lisy.js

---
+++ react_lisy.js
@@ -0,0 +1,17 @@
+#list
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const numbers = [1,2,3,4,5];
+
+const updatedNums = numbers.map((number)=>{
+	return <li>{number}</li>;
+});
+
+ReactDOM.render(
+	<ul>
+		{updatedNums}
+	</ul>,
+	document.getElementById('root')
+);
